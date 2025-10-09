7dd1160e8559bcc9c3e948390218383e7bef58ac
Hiya <hiyagurbani12@gmail.com>
2025-10-09 22:15:30 +0530

    Added function to calculate total and average marks and grade

--- performanceAnalyzer.c
+++ performanceAnalyzer.c
@@ -21,6 +21,34 @@ void getStudentDetails(StudentDetails students[], const int index){
     scanf ("%u", &students[index].marksSubject3);
 }
 
+void calculateTotalAndAverageMarks(StudentDetails students[], const int index){
+    students[index].totalMarks = students[index].marksSubject1 + students[index].marksSubject2 + students[index].marksSubject3;
+    students[index].averageMarks = students[index].totalMarks / 3.0;
+}
+
+void calculateGrade(StudentDetails students[], const int index){
+    if (students[index].averageMarks >= 85)
+    {
+        students[index].grade = 'A';
+    } 
+    else if (students[index].averageMarks >= 70)
+    {
+        students[index].grade = 'B';
+    }
+    else if (students[index].averageMarks >= 50)
+    {
+        students[index].grade = 'C';
+    }
+    else if (students[index].averageMarks >= 35)
+    {
+        students[index].grade = 'D';
+    }
+    else 
+    {
+        students[index].grade = 'F';
+    }
+}
+
 int main(){
     unsigned int numberOfStudents;
     scanf ("%d", &numberOfStudents);
@@ -30,6 +58,10 @@ int main(){
     for (int index = 0; index < numberOfStudents; index++)
     {
         getStudentDetails(students, index);
+        
+        calculateTotalAndAverageMarks(students, index);
+
+        calculateGrade(students, index);
     }
 
     return 0;
