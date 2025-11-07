6c6a16d50d4b2d85592ef20c0485d8d8750a1e90
Hiya <hiyagurbani12@gmail.com>
2025-11-07 12:10:50 +0530

    Added functionality of rmdir and pwd and also updated path display by cd

--- VirtualFileSystem.c
+++ VirtualFileSystem.c
@@ -7,10 +7,18 @@
 #define BLOCK_SIZE 16 //Column Number
 #define NUMBER_OF_BLOCKS 5 //Row Number 
 
-#define NAME_SIZE 100
+#define NAME_SIZE 50
 #define MAX_BLOCKS_PER_FILE 5
 
 #define INPUT_SIZE 100
+
+#define PATH_SIZE 100
+
+typedef enum {
+    RMDIR_SUCCESS,
+    RMDIR_NOT_FOUND,
+    RMDIR_NOT_EMPTY
+} RmdirStatus;
 
 typedef struct FreeBlock {
     int data;
@@ -120,7 +128,7 @@ bool makeDirectory(FileNode *cwd, char *directoryName) {
             printf("Directory Name %s already exists.\n", directoryName);
             return false;
         }
-        
+
         FileNode *temp = cwd->child;
         while (temp->next != cwd->child)
         {
@@ -131,6 +139,51 @@ bool makeDirectory(FileNode *cwd, char *directoryName) {
     }
 
     return true;
+}
+
+RmdirStatus removeDirectory(FileNode* cwd, char* directoryName) {
+    if (cwd->child == NULL)
+    {
+        return RMDIR_NOT_FOUND;
+    }
+
+    FileNode* prev = NULL, *curr = cwd->child;
+
+    do
+    {
+        if (strcmp(curr->name, directoryName) == 0)
+        {
+            if (curr->child)
+            {
+                return RMDIR_NOT_EMPTY; //Child Exists
+            }
+            if (curr->next == curr)
+            {
+                cwd->child = NULL;
+            }
+            else if (prev == NULL)
+            {
+                FileNode* last = cwd->child;
+                while (last->next != cwd->child)
+                {
+                    last = last->next;
+                }
+                last->next = curr->next;
+                cwd->child = curr->next;
+            }
+            else
+            {
+                prev->next = curr->next;
+            }
+            free(curr);
+            curr = NULL;
+            return RMDIR_SUCCESS;
+        }
+        prev = curr;
+        curr = curr->next;
+    } while (curr != cwd->child);
+
+    return RMDIR_NOT_FOUND;
 }
 
 bool changeDirectory(FileNode** cwd, char *directoryName) {
@@ -166,7 +219,7 @@ bool cdParent(FileNode** cwd) {
 void listChildren(FileNode* cwd) {
     if (cwd->child == NULL)
     {
-        printf("(empty)");
+        printf("(empty)\n");
         return;
     }
 
@@ -177,6 +230,24 @@ void listChildren(FileNode* cwd) {
         printf("%s\n", temp->name);
         temp = temp->next;
     } while (temp != cwd->child);
+}
+
+void workingDirectory(FileNode* cwd, char *path) {
+    if (cwd->parent == NULL) {
+        return;
+    }
+    
+    workingDirectory(cwd->parent, path);
+    strcat(path, "/");
+    strcat(path, cwd->name);
+}
+
+void getWorkingDirectory(FileNode* cwd, char *path) {
+    workingDirectory(cwd, path);
+    if (strcmp(path, "") == 0)
+    {
+        strcpy(path, "/");
+    }
 }
 
 int main() {
@@ -199,6 +270,8 @@ int main() {
         fgets(input, INPUT_SIZE, stdin);
         input[strcspn(input, "\n")] = '\0';
 
+        char path[PATH_SIZE] = "";
+
         if (strncmp(input, "mkdir ", 6) == 0) 
         {
             if (makeDirectory(cwd, input + 6))
@@ -211,7 +284,8 @@ int main() {
         {
             if (changeDirectory(&cwd, input + 3))
             {
-                printf("Moved to /%s\n", cwd->name);
+                getWorkingDirectory(cwd, path);
+                printf("Moved to %s\n", path);
             }
             else 
             {
@@ -223,7 +297,8 @@ int main() {
         {
             if (cdParent(&cwd))
             {
-                printf("Moved to /%s\n", cwd->name);
+                getWorkingDirectory(cwd, path);
+                printf("Moved to %s\n", path);
             }
             else 
             {
@@ -234,6 +309,29 @@ int main() {
         else if (strncmp(input, "ls", 2) == 0)
         {
             listChildren(cwd);
+        }
+
+        else if (strncmp(input, "rmdir ", 6) == 0)
+        {
+            RmdirStatus status = removeDirectory(cwd, input + 6);
+            if (status == RMDIR_SUCCESS)
+            {
+                printf("Directory removed successfuly.\n");
+            }
+            else if (status == RMDIR_NOT_FOUND)
+            {
+                printf("Directory '%s' does not exist under '%s'.\n", input + 6, cwd->name);
+            }
+            else 
+            {
+                printf("Failed to remove '%s': directory is not empty.\n", input + 6);
+            }
+        }
+
+        else if (strncmp(input, "pwd", 3) == 0)
+        {
+            getWorkingDirectory(cwd, path);
+            printf("%s\n", path);
         }
 
         else {
