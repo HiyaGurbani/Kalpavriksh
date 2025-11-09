e4cf68197172061733e9c0fa8f695d4d04d83040
Hiya <hiyagurbani12@gmail.com>
2025-11-09 07:37:02 +0530

    Increased Readability by breaking the handleCommand function to multiple functions

--- VirtualFileSystem.c
+++ VirtualFileSystem.c
@@ -580,47 +580,134 @@ void freeFileNodes(FileNode* node) {
     } while(current != NULL && current != start);
 }
 
-void handleCommand(char *input, FileNode **currentDirectory, FreeBlock **freeBlockHead, FreeBlock **freeBlockTail, 
-                    FileNode *root, char (*virtualDisk)[BLOCK_SIZE] ) 
-{
+void handleMkdir(char* input, FileNode** currentDirectory) {
+    char* directoryName = input + strlen(CMD_MKDIR);
+    if (createNodeInDirectory(*currentDirectory, directoryName, false))
+    {
+        printf("Directory '%s' created successfully.\n", directoryName);
+    }
+    else
+    {
+        printf("Directory '%s' already exists.\n", directoryName);
+    }
+}
+
+void handleCd(char* input, FileNode** currentDirectory, char* path) {
+    char* directoryName = input + strlen(CMD_CD);
+    if (changeDirectory(currentDirectory, directoryName)) 
+    {
+        getWorkingDirectory(*currentDirectory, path);
+        printf("Moved to %s\n", path);
+    } else 
+    {
+        printf("Directory '%s' not found.\n", directoryName);
+    }
+}
+
+void handleCdParent(FileNode** currentDirectory, char* path) {
+    if (cdParent(currentDirectory)) 
+    {
+        getWorkingDirectory(*currentDirectory, path);
+        printf("Moved to %s\n", path);
+    } else 
+    {
+        printf("Already in root.\n");
+    }
+}
+
+void handleRmdir(char* input, FileNode** currentDirectory) {
+    char* directoryName = input + strlen(CMD_RMDIR);
+    RmdirStatus status = removeDirectory(*currentDirectory, directoryName);
+    if (status == RMDIR_SUCCESS) 
+    {
+        printf("Directory removed successfully.\n");
+    }
+    else if (status == RMDIR_NOT_FOUND)
+    {
+        printf("Directory '%s' not found.\n", directoryName);
+    }
+    else
+    {
+        printf("Directory not empty.\n");
+    }
+}
+
+void handleCreate(char* input, FileNode** currentDirectory) {
+    char* fileName = input + strlen(CMD_CREATE);
+    if (createNodeInDirectory(*currentDirectory, fileName, true)) 
+    {
+        printf("File '%s' created successfully.\n", fileName);
+    }
+    else
+    {
+        printf("File '%s' already exists.\n", fileName);
+    }
+}
+
+void handleWrite(char* input, FileNode** currentDirectory, FreeBlock **freeBlockHead, 
+    FreeBlock **freeBlockTail, char (*virtualDisk)[BLOCK_SIZE]) {
+    char* remainingInput = input + strlen(CMD_WRITE);
+    char fileName[NAME_SIZE] = "";
+    char text[TEXT_SIZE] = "";
+
+    if (getDataFromString(remainingInput, fileName, text)) 
+    {
+        if (writeDataIntoFile(*currentDirectory, fileName, text, freeBlockHead, freeBlockTail, virtualDisk))
+        {
+            printf("Data written successfully (%lu bytes)\n", strlen(text));
+        }
+    } else 
+    {
+        printf("Invalid write command.\n");
+    }
+}
+
+void handleRead(char* input, FileNode** currentDirectory, char (*virtualDisk)[BLOCK_SIZE]) {
+    char* fileName = input + strlen(CMD_READ);
+    if (!readFile(*currentDirectory, fileName, virtualDisk))
+    {
+        printf("File not found.\n");
+    }    
+}
+
+void handleDelete(char* input, FileNode** currentDirectory, FreeBlock **freeBlockHead, FreeBlock **freeBlockTail) {
+    char* fileName = input + strlen(CMD_DELETE);
+    if (deleteFile(*currentDirectory, fileName, freeBlockHead, freeBlockTail))
+    {
+        printf("File deleted successfully.\n");
+    }
+    else
+    {
+        printf("File not found.\n");
+    }
+}
+
+void handleExit(FreeBlock **freeBlockHead, FreeBlock **freeBlockTail, 
+    FileNode *root, char (*virtualDisk)[BLOCK_SIZE]) {
+    free(virtualDisk);
+    freeFreeBlocks(freeBlockHead, freeBlockTail);
+    freeFileNodes(root);
+    printf("Memory released. Exiting program...\n");
+    exit(0);
+}
+
+void handleCommand(char *input, FileNode **currentDirectory, FreeBlock **freeBlockHead, 
+    FreeBlock **freeBlockTail, FileNode *root, char (*virtualDisk)[BLOCK_SIZE] ) {
     char path[PATH_SIZE] = "";
 
     if (strncmp(input, CMD_MKDIR, strlen(CMD_MKDIR)) == 0) 
     {
-        char* directoryName = input + strlen(CMD_MKDIR);
-        if (createNodeInDirectory(*currentDirectory, directoryName, false))
-        {
-            printf("Directory '%s' created successfully.\n", directoryName);
-        }
-        else
-        {
-            printf("Directory '%s' already exists.\n", directoryName);
-        }
+        handleMkdir(input, currentDirectory);
     }
 
     else if (strncmp(input, CMD_CD, strlen(CMD_CD)) == 0) 
     {
-        char* directoryName = input + strlen(CMD_CD);
-        if (changeDirectory(currentDirectory, directoryName)) 
-        {
-            getWorkingDirectory(*currentDirectory, path);
-            printf("Moved to %s\n", path);
-        } else 
-        {
-            printf("Directory '%s' not found.\n", directoryName);
-        }
+        handleCd(input, currentDirectory, path);
     }
 
     else if (strcmp(input, CMD_CD_PARENT) == 0) 
     {
-        if (cdParent(currentDirectory)) 
-        {
-            getWorkingDirectory(*currentDirectory, path);
-            printf("Moved to %s\n", path);
-        } else 
-        {
-            printf("Already in root.\n");
-        }
+        handleCdParent(currentDirectory, path);
     }
 
     else if (strcmp(input, CMD_LS) == 0) 
@@ -630,20 +717,7 @@ void handleCommand(char *input, FileNode **currentDirectory, FreeBlock **freeBlo
 
     else if (strncmp(input, CMD_RMDIR, strlen(CMD_RMDIR)) == 0) 
     {
-        char* directoryName = input + strlen(CMD_RMDIR);
-        RmdirStatus status = removeDirectory(*currentDirectory, directoryName);
-        if (status == RMDIR_SUCCESS) 
-        {
-            printf("Directory removed successfully.\n");
-        }
-        else if (status == RMDIR_NOT_FOUND)
-        {
-            printf("Directory '%s' not found.\n", directoryName);
-        }
-        else
-        {
-            printf("Directory not empty.\n");
-        }
+        handleRmdir(input, currentDirectory);
     }
 
     else if (strcmp(input, CMD_PWD) == 0) 
@@ -654,55 +728,22 @@ void handleCommand(char *input, FileNode **currentDirectory, FreeBlock **freeBlo
 
     else if (strncmp(input, CMD_CREATE, strlen(CMD_CREATE)) == 0) 
     {
-        char* fileName = input + strlen(CMD_CREATE);
-        if (createNodeInDirectory(*currentDirectory, fileName, true)) 
-        {
-            printf("File '%s' created successfully.\n", fileName);
-        }
-        else
-        {
-            printf("File '%s' already exists.\n", fileName);
-        }
+        handleCreate(input, currentDirectory);
     }
 
     else if (strncmp(input, CMD_WRITE, strlen(CMD_WRITE)) == 0) 
     {
-        char* remainingInput = input + strlen(CMD_WRITE);
-        char fileName[NAME_SIZE] = "";
-        char text[TEXT_SIZE] = "";
-
-        if (getDataFromString(remainingInput, fileName, text)) 
-        {
-            if (writeDataIntoFile(*currentDirectory, fileName, text, freeBlockHead, freeBlockTail, virtualDisk))
-            {
-                printf("Data written successfully (%lu bytes)\n", strlen(text));
-            }
-        } else 
-        {
-            printf("Invalid write command.\n");
-        }
+        handleWrite(input, currentDirectory, freeBlockHead, freeBlockTail, virtualDisk);
     }
 
     else if (strncmp(input, CMD_READ, strlen(CMD_READ)) == 0) 
     {
-        char* fileName = input + strlen(CMD_READ);
-        if (!readFile(*currentDirectory, fileName, virtualDisk))
-        {
-            printf("File not found.\n");
-        }
+        handleRead(input, currentDirectory, virtualDisk);
     }
 
     else if (strncmp(input, CMD_DELETE, strlen(CMD_DELETE)) == 0) 
     {
-        char* fileName = input + strlen(CMD_DELETE);
-        if (deleteFile(*currentDirectory, fileName, freeBlockHead, freeBlockTail))
-        {
-            printf("File deleted successfully.\n");
-        }
-        else
-        {
-            printf("File not found.\n");
-        }
+        handleDelete(input, currentDirectory, freeBlockHead, freeBlockTail);
     }
 
     else if (strcmp(input, CMD_DF) == 0) 
@@ -712,11 +753,7 @@ void handleCommand(char *input, FileNode **currentDirectory, FreeBlock **freeBlo
 
     else if (strcmp(input, CMD_EXIT) == 0) 
     {
-        free(virtualDisk);
-        freeFreeBlocks(freeBlockHead, freeBlockTail);
-        freeFileNodes(root);
-        printf("Memory released. Exiting program...\n");
-        exit(0);
+        handleExit(freeBlockHead, freeBlockTail, root, virtualDisk);
     }
 
     else 
